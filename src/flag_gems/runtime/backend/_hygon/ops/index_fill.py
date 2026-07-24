80df5c18eb551d674ea789843fd4ea8d7455bb60
baohaohui <1184979877@qq.com>
2026-07-24 14:17:13 +0800

    fix(hygon): handle strided index fill paths

--- src/flag_gems/runtime/backend/_hygon/ops/index_fill.py
+++ src/flag_gems/runtime/backend/_hygon/ops/index_fill.py
@@ -4,15 +4,15 @@ import torch
 import triton
 import triton.language as tl
 
-from flag_gems.ops.index_fill import _prepare_index, _prepare_tensor_value
+from flag_gems.ops.index_fill import (
+    _index_fill_impl,
+    _prepare_index,
+    _prepare_tensor_value,
+)
 from flag_gems.runtime import torch_device_fn
 from flag_gems.utils import libentry
 
 logger = logging.getLogger(__name__)
-
-_FALLBACK_KEYSET = torch._C.DispatchKeySet(
-    torch._C.DispatchKey.CompositeExplicitAutograd
-)
 
 
 @libentry()
@@ -58,50 +58,6 @@ def index_fill_contiguous_kernel(
     tl.store(out + out_offsets, fill_value, mask=mask & valid_index)
 
 
-def _native_clone(inp):
-    return torch.ops.aten.clone.default.redispatch(_FALLBACK_KEYSET, inp)
-
-
-def _native_copy_(out, inp):
-    return torch.ops.aten.copy_.default.redispatch(_FALLBACK_KEYSET, out, inp, False)
-
-
-def _native_index_fill_scalar(inp, dim, index, value):
-    return torch.ops.aten.index_fill.int_Scalar.redispatch(
-        _FALLBACK_KEYSET, inp, dim, index, value
-    )
-
-
-def _native_index_fill_scalar_(inp, dim, index, value):
-    return torch.ops.aten.index_fill_.int_Scalar.redispatch(
-        _FALLBACK_KEYSET, inp, dim, index, value
-    )
-
-
-def _native_index_fill_tensor(inp, dim, index, value):
-    return torch.ops.aten.index_fill.int_Tensor.redispatch(
-        _FALLBACK_KEYSET, inp, dim, index, value
-    )
-
-
-def _native_index_fill_tensor_(inp, dim, index, value):
-    return torch.ops.aten.index_fill_.int_Tensor.redispatch(
-        _FALLBACK_KEYSET, inp, dim, index, value
-    )
-
-
-def _native_index_fill_scalar_out(inp, dim, index, value, out):
-    return torch.ops.aten.index_fill.int_Scalar_out.redispatch(
-        _FALLBACK_KEYSET, inp, dim, index, value, out=out
-    )
-
-
-def _native_index_fill_tensor_out(inp, dim, index, value, out):
-    return torch.ops.aten.index_fill.int_Tensor_out.redispatch(
-        _FALLBACK_KEYSET, inp, dim, index, value, out=out
-    )
-
-
 def _block_inner(inner_size):
     return min(1024, triton.next_power_of_2(max(1, inner_size)))
 
@@ -137,69 +93,62 @@ def _index_fill_contiguous_(out, dim, index, value, value_is_tensor):
 
 def _prepare_hcu_index(inp, dim, index):
     dim, index = _prepare_index(inp, dim, index)
+    if index.numel() > 0:
+        dim_size = inp.size(dim)
+        min_index, max_index = torch.aminmax(index)
+        if int(min_index.item()) < -dim_size or int(max_index.item()) >= dim_size:
+            raise IndexError("index out of range in self")
     return dim, index.contiguous()
+
+
+def _index_fill_(out, dim, index, value, value_is_tensor):
+    if out.is_contiguous():
+        return _index_fill_contiguous_(out, dim, index, value, value_is_tensor)
+    return _index_fill_impl(out, dim, index, value, value_is_tensor)
 
 
 def index_fill_scalar(inp, dim, index, value):
     logger.debug("GEMS_HYGON INDEX_FILL SCALAR")
     dim, index = _prepare_hcu_index(inp, dim, index)
-    if not inp.is_contiguous():
-        return _native_index_fill_scalar(inp, dim, index, value)
-    out = _native_clone(inp)
-    return _index_fill_contiguous_(out, dim, index, value, False)
+    out = inp.clone(memory_format=torch.preserve_format)
+    return _index_fill_(out, dim, index, value, False)
 
 
 def index_fill_scalar_(inp, dim, index, value):
     logger.debug("GEMS_HYGON INDEX_FILL_ SCALAR")
     dim, index = _prepare_hcu_index(inp, dim, index)
-    if not inp.is_contiguous():
-        return _native_index_fill_scalar_(inp, dim, index, value)
-    return _index_fill_contiguous_(inp, dim, index, value, False)
+    return _index_fill_(inp, dim, index, value, False)
 
 
 def index_fill_tensor(inp, dim, index, value):
     logger.debug("GEMS_HYGON INDEX_FILL TENSOR")
     dim, index = _prepare_hcu_index(inp, dim, index)
     value_is_tensor, value = _prepare_tensor_value(inp, value)
-    if not inp.is_contiguous():
-        if value_is_tensor:
-            return _native_index_fill_tensor(inp, dim, index, value)
-        return _native_index_fill_scalar(inp, dim, index, value)
-    out = _native_clone(inp)
-    return _index_fill_contiguous_(out, dim, index, value, value_is_tensor)
+    out = inp.clone(memory_format=torch.preserve_format)
+    return _index_fill_(out, dim, index, value, value_is_tensor)
 
 
 def index_fill_tensor_(inp, dim, index, value):
     logger.debug("GEMS_HYGON INDEX_FILL_ TENSOR")
     dim, index = _prepare_hcu_index(inp, dim, index)
     value_is_tensor, value = _prepare_tensor_value(inp, value)
-    if not inp.is_contiguous():
-        if value_is_tensor:
-            return _native_index_fill_tensor_(inp, dim, index, value)
-        return _native_index_fill_scalar_(inp, dim, index, value)
-    return _index_fill_contiguous_(inp, dim, index, value, value_is_tensor)
+    return _index_fill_(inp, dim, index, value, value_is_tensor)
 
 
 def index_fill_scalar_out(inp, dim, index, value, *, out):
     logger.debug("GEMS_HYGON INDEX_FILL SCALAR_OUT")
     dim, index = _prepare_hcu_index(inp, dim, index)
-    if not inp.is_contiguous() or not out.is_contiguous():
-        return _native_index_fill_scalar_out(inp, dim, index, value, out)
     if tuple(out.shape) != tuple(inp.shape):
         out.resize_(inp.shape)
-    _native_copy_(out, inp)
-    return _index_fill_contiguous_(out, dim, index, value, False)
+    out.copy_(inp)
+    return _index_fill_(out, dim, index, value, False)
 
 
 def index_fill_tensor_out(inp, dim, index, value, *, out):
     logger.debug("GEMS_HYGON INDEX_FILL TENSOR_OUT")
     dim, index = _prepare_hcu_index(inp, dim, index)
     value_is_tensor, value = _prepare_tensor_value(inp, value)
-    if not inp.is_contiguous() or not out.is_contiguous():
-        if value_is_tensor:
-            return _native_index_fill_tensor_out(inp, dim, index, value, out)
-        return _native_index_fill_scalar_out(inp, dim, index, value, out)
     if tuple(out.shape) != tuple(inp.shape):
         out.resize_(inp.shape)
-    _native_copy_(out, inp)
-    return _index_fill_contiguous_(out, dim, index, value, value_is_tensor)
+    out.copy_(inp)
+    return _index_fill_(out, dim, index, value, value_is_tensor)
